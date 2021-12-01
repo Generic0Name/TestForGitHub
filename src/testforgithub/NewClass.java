bf151f00bdc841870a42872b88aff29ff1578420
Generic0Name <95368293+Generic0Name@users.noreply.github.com>
2021-12-01 08:36:42 -0800

    Initial commit

---
+++ src/testforgithub/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testforgithub;
+
+/**
+ *
+ * @author 140001221
+ */
+public class NewClass {
+    //Another test
+}
---
+++ src/testforgithub/TestForGitHub.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testforgithub;
+
+/**
+ *
+ * @author 140001221
+ */
+public class TestForGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Does it work
+        System.out.println("Test Here");
+        //Maybe
+    }
+    
+}
